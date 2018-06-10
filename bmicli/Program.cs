35fad6d1fe213c5b4506ca99e3321e97fa66ce5e
George Ponick <gponick@gmail.com>
2018-06-09 20:39:19 -0400

    work init/list/update and default to update *

--- bmicli/Program.cs
+++ bmicli/Program.cs
@@ -11,38 +11,6 @@ namespace bmicli
     class Program
     {
         public static List<Mod> mods = new List<Mod>();
-        public void DoStuff()
-        {
-            BMILib.Mod mod = BMILib.Mod.LoadFromDirectory(@"C:\Users\George\source\repos\BTechMods\temp\Mods-Non-Rogue-2018-05-31\BTMLColorLOSMod");
-            //BMILib.Mod mod = BMILib.Mod.LoadFromDirectory(@"C:\Program Files (x86)\Steam\steamapps\common\BATTLETECH\Mods\DirtyBallisticFix");
-            Console.WriteLine($"Mod: {mod.Name} from [{mod.ModDirectory}] is version: {mod.Version}");
-            Console.WriteLine($"{mod.Author} wrote it and it lives at {mod.Website}");
-            if (mod.JSONDirectories.Count > 0)
-            {
-                foreach (var key in mod.JSONDirectories.Keys)
-                {
-                    Console.WriteLine($"[{key}]");
-                    foreach (var thing in mod.JSONDirectories[key])
-                    {
-                        Console.WriteLine($"\t\tcontained: {thing.Item1} file");
-                    }
-                }
-            }
-            if (mod.Releases.Count > 0)
-            {
-                foreach (var release in mod.Releases.OrderByDescending(l => l.CreatedAt))
-                {
-                    Console.WriteLine($"Version {release.TagName} released at {release.CreatedAt}. Download at: {release.Assets[0].BrowserDownloadUrl}");
-                }
-                Console.WriteLine($"Needs Update????? {mod.NeedsUpdate()}");
-                if (mod.NeedsUpdate())
-                {
-                    mod.fetchLatestReleaseBytes();
-                    System.IO.File.WriteAllBytes(@"C:\Users\George\source\repos\BTechMods\temp\Mods-Non-Rogue-2018-05-31\BTMLColorLOSMod\" + mod.LatestRelease.Assets[0].Name, mod.LatestReleaseFile);
-                    Console.WriteLine("Update file written");
-                }
-            }
-        }
         static void Initialize(bool fakecli = true)
         {
             mods.Clear();
@@ -50,11 +18,9 @@ namespace bmicli
             {
                 Console.WriteLine(@"C:\BattleTech\Mods\> bmi init");
             }
-            Console.Write(@"bmi> Initializing Mods from [C:\Battletech\Mods\]...");
+            System.IO.DirectoryInfo di = new DirectoryInfo(@".");
+            Console.Write($"bmi> Initializing Mods from [{di.FullName}]...");
 
-            //System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(@"C:\Users\George\source\repos\BTechMods\temp\RogueTech-79-0-9856HF1\");
-            System.IO.DirectoryInfo di = new DirectoryInfo(@"C:\Users\George\source\repos\BTechMods\temp\BMITest\");
-            //System.IO.DirectoryInfo di = new DirectoryInfo(".");
             mods = new List<Mod>();
             foreach (DirectoryInfo d in di.EnumerateDirectories())
             {
@@ -88,7 +54,8 @@ namespace bmicli
             {
                 Console.WriteLine($"C:\\BattleTech\\Mods\\> bmi init {modName}");
             }
-            Console.Write($"bmi> Initializing {modName} from [C:\\Battletech\\Mods\\]...");
+            System.IO.DirectoryInfo di = new DirectoryInfo(@".");
+            Console.Write($"bmi> Initializing {modName} from [{di.FullName}]...");
 
             Mod toRemove = null;
             foreach(Mod m in mods)
@@ -107,9 +74,10 @@ namespace bmicli
             Console.WriteLine(@" SUCCESS!");
         }
 
-        public static void List()
+        public static void List(bool fakecli = false)
         {
-            Console.WriteLine(@"C:\BattleTech\Mods\> bmi list");
+            if(fakecli)
+                Console.WriteLine(@"C:\BattleTech\Mods\> bmi list");
             Console.WriteLine($"{"Name",-30}|{"Version",-10}|{"Directory",-30}|{"Needs Update"}");
             Console.WriteLine($"------------------------------+----------+------------------------------+--------------------");
             foreach (Mod m in mods.Where(mmm=>mmm != null).OrderBy(mm=>mm.Name))
@@ -125,9 +93,10 @@ namespace bmicli
             }
         }
 
-        public static void Update(string updateString = "*")
+        public static void Update(string updateString = "*", bool fakecli = false)
         {
-            Console.WriteLine($"C:\\BattleTech\\Mods\\> bmi update  {updateString}");
+            if(fakecli)
+                Console.WriteLine($"C:\\BattleTech\\Mods\\> bmi update  {updateString}");
             foreach (Mod m in mods.Where(mm => mm.NeedsUpdate()))
             {
                 if(updateString == "*" || m.Name == updateString)
@@ -141,24 +110,37 @@ namespace bmicli
         {
             if(args.Count() == 0)
             {
-                Initialize();
-                List();
-                Update();
-                List();
+                Initialize(false);
+                List(false);
+                Update("*",false);
+                List(false);
             }
-            if (args[0] == "init")
+            else if (args[0] == "init")
             {
                 Initialize();
-                
             }
-            List();
-            //Update("BTMLAddBindableEscapeKey");
-            //Update("CommanderPortraitLoader");
-            //Update("RandomCampaignStart");
-            Update();
-            //Update("DynModLib");
-
-            List();
+            else if (args[0] == "list")
+            {
+                Initialize(false);
+                List(false);
+            }
+            else if (args[0] == "update")
+            {
+                if(args.Count() > 1)
+                {
+                    Initialize(false);
+                    List(false);
+                    Update(args[1], false);
+                    List(false);
+                }
+                else
+                {
+                    Initialize(false);
+                    List(false);
+                    Update("*", false);
+                    List(false);
+                }
+            }
         }
     }
 }
